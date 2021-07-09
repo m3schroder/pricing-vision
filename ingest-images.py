68a0620cbfa6fd49a413f7b94fc9100103ad2e50
Matt Schroder <m3schroder@gmail.com>
2021-07-09 10:28:04 -0500

    project start

---
+++ ingest-images.py
@@ -0,0 +1,10 @@
+import neccessary libraries
+import script I used previously for downloading web images
+
+def readWebImages:
+    searchQueries = some list of words
+    
+    use imported script to search and download based on search queries
+
+    :
+    
